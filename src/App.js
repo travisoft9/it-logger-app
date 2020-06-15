f1209ae0fc1f2dc2c1fd8a17ea5f09a68241910a
Travis Richards <travisoft9@gmail.com>
2020-06-15 17:33:12 -0400

    added log material UI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
 
 const App = () => {
   useEffect(() => {
@@ -15,6 +16,7 @@ const App = () => {
       <Fragment>
         <SearchBar />
         <div className='container'>
+          <AddBtn />
           <Logs />
         </div>
       </Fragment>
---
+++ src/components/logs/AddLogModal.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+const AddLogModal = () => {
+  const [message, setMessage] = useState('');
+  const [attention, setAttention] = useState(false);
+  const [tech, setTech] = useState('');
+
+  return (
+    <div id='add-log-modal' className='modal' style={modalStyle}>
+      <div className='modal-content'>
+        <h4>Enter System Log</h4>
+        <div className='row'>
+          <div className='input-field'>
+            <input
+              type='text'
+              name='message'
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <label htmlFor='message' className='active'>
+              Log Message
+            </label>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='input-field'>
+            <select
+              className='browser-default'
+              name='tech'
+              value={tech}
+              onChange={(e) => setTech(e.target.value)}
+            >
+              <option value='' disabled>
+                Select Technician
+              </option>
+              <option value='John Doe'>John Doe</option>
+              <option value='Sarah Wilson'>Sarah Smith</option>
+              <option value='Sam Smith'>John Doe</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const modalStyle = {
+  width: '75%',
+  height: '75%',
+};
+
+export default AddLogModal;
