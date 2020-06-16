45657c2e32efb61d3dae8ba0e31dfa52e37cd935
Travis Richards <travisoft9@gmail.com>
2020-06-16 00:21:25 -0400

    finished

--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -2,17 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { deleteTech } from '../../actions/techActions';
+import M from 'materialize-css/dist/js/materialize.min.js';
 
 const TechItem = ({ tech: { id, firstName, lastName }, deleteTech }) => {
+  const onDelete = () => {
+    deleteTech(id);
+    M.toast({ html: 'This technician has been deleted.' });
+  };
+
   return (
     <li className='collection-item'>
       <div>
         {firstName} {lastName}
-        <a
-          href='#!'
-          className='secondary-content'
-          onClick={() => deleteTech(id)}
-        >
+        <a href='#!' className='secondary-content' onClick={onDelete}>
           <i className='material-icons grey-text'>delete</i>
         </a>
       </div>
